69acfdbd21d9a8628b803e4ce527b40041f7c24c
madelinenlee <madelinenlee@gmail.com>
2019-02-25 16:03:55 -0500

    general debugging

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,36 +7,50 @@ Created on Wed Feb 20 14:45:27 2019
 """
 
 import dotenv as de
-import json
-import requests
-import os
-import pandas as pd
-import datetime as datetime
-from statistics import mean
+import os as os
 
 de.load_dotenv()
 
 api_key = os.environ.get('MY_API_KEY')
+plotly_api_key = os.environ.get('plotly_api_key')
+
+import json
+import requests
+
+import pandas as pd
+import datetime as datetime
+
+import plotly
+from plotly import tools
+plotly.tools.set_credentials_file(username='madelinelee', api_key=plotly_api_key)
+
+import plotly.plotly as py
+
+
+import plotly.graph_objs as go
+#from plotly.tools import FigureFactory as FF
+
 
 def user_input():
     stock_list = []
-    valid = True
+    #valid = True
     symbol = ''
         
     while symbol != 'DONE':
-        symbol = input('Please input desired stock symbol or DONE when done: ')
+        symbol = input('Please input desired stock symbol (e.g. MSFT) or DONE when done: ')
         
         if symbol == 'DONE':
             stock_list = list(set(stock_list))
+            print(stock_list)
             return(stock_list)
         
         if any(char.isdigit() for char in symbol):
             #valid = False
-            print('sorry, invalid stock symbol... please try again')
+            print('sorry, invalid stock symbol (chars only) ... please try again')
             
-        elif len(symbol) > 4:
+        elif len(symbol) != 4:
             #valid = False
-            print('sorry, invalid stock symbol ... please try again')
+            print('sorry, invalid stock symbol (wrong number of characters) ... please try again')
         
         else:
             stock_list.append(symbol)
@@ -52,6 +66,8 @@ def get_stock_data(symbol):
     parsed_response = json.loads(response.text)
     
     return(parsed_response)
+
+#
     
 #function to validate that the dictionary was correctly loaded
 def validate_stock_data(stock_dictionary):
@@ -71,8 +87,10 @@ def print_stock_data(stock_dictionary):
               ', ' + temp_path['3. low'] + ', ' + temp_path['4. close'] +
               ', ' + temp_path['5. volume'])
 
-def create_dataframe(stock_dictionary):
+def create_dataframe(symbol, stock_dictionary):
     #columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+    #stock_dictionary = get_stock_data(symbol)
+    #print(stock_dictionary)
     timestamp = []
     open_price = []
     high = []
@@ -80,7 +98,9 @@ def create_dataframe(stock_dictionary):
     close = []
     volume = []
     
+    #print('creating empty dataframe...')
     temp_frame = pd.DataFrame()
+    
     for i in stock_dictionary['Time Series (Daily)']:
         temp_path = stock_dictionary['Time Series (Daily)'][i]
         timestamp.append(i)
@@ -89,10 +109,7 @@ def create_dataframe(stock_dictionary):
         low.append(temp_path['3. low'])
         close.append(temp_path['4. close'])
         volume.append(temp_path['5. volume'])
-        
-        '''print(i + ', ' + temp_path['1. open'] + ', ' + temp_path['2. high'] +
-              ', ' + temp_path['3. low'] + ', ' + temp_path['4. close'] +
-              ', ' + temp_path['5. volume'])'''
+
     
     timestamp = pd.Series(timestamp)
     open_price = pd.Series(open_price)
@@ -101,6 +118,7 @@ def create_dataframe(stock_dictionary):
     close = pd.Series(close)
     volume = pd.Series(volume)
     
+    temp_frame['symbol'] = symbol
     temp_frame['timestamp'] = timestamp
     temp_frame['open_price'] = open_price
     temp_frame['high'] = high
@@ -109,11 +127,13 @@ def create_dataframe(stock_dictionary):
     temp_frame['volume'] = volume
 
     return(temp_frame)
-    
+   
+
 def to_csv(symbol, data_frame):
-    #can't figure out how to write this to a folder named 'data', keeps giving me 
-    data_frame.to_csv('/Users/madeline/Desktop/robo-advisor-project/data/'+
-                      symbol + '-' + data_frame['timestamp'][0] +'.csv')
+    #os.getcwd()
+    csv_file_path = os.path.join(os.path.dirname('__file__'),
+                                 "data", symbol + '-' + data_frame['timestamp'][0] +'.csv')
+    data_frame.to_csv(csv_file_path)
 
 def calculate_max(data_frame):
     temp_frame = data_frame
@@ -159,47 +179,97 @@ def explanation(recommendation, data_frame):
               str(calculate_min(data_frame)) + '), therefore, it is too risky to buy.')   
 
 def printout(symbol, data_frame):
-    date = datetime.datetime.now()
+    date_now = datetime.datetime.now()
     recent_close = data_frame['close'][0]
     recommendation = recommend_alg(data_frame)
     
     print('Stock: ' + symbol)
-    print('Run at: ' + str(date.hour) + ':' + str(date.minute) + ', ' + 
-          str(date.year) + '-' + str(date.month) + '-' + str(date.day))
+    print('Run at: ' + str(date_now.hour) + ':' + str(date_now.minute) + ', ' + 
+          str(date_now.year) + '-' + str(date_now.month) + '-' + str(date_now.day))
     print('Latest data from: ' + data_frame['timestamp'][0])
     print('Latest closing price: '+ to_usd(recent_close))
     print('Recent high price: ' + to_usd(calculate_max(data_frame)))
     print('Recent low price: ' + to_usd(calculate_min(data_frame)))
     print('Recommendation:', print_recommendation(recommendation))
     print('Explanation:', explanation(recommendation, data_frame))
+    print('\n')
 
-'''
-symbol = 'MSFT'
-parsed = get_stock_data('MSFT')
-msft_data = create_dataframe(parsed)
-test_close = msft_data['close'][0]
-msft_high = calculate_max(msft_data)
-msft_low = calculate_min(msft_data)
-recommend_alg(msft_data)
-to_csv('MSFT', msft_data)
-printout('MSFT', msft_data)'''
+def append_data_frame(data_list):
+
+    final_frame = pd.concat(data_list).reset_index(drop=True)
+    
+    return(final_frame)
+
+stock_list = ['MSFT','AMZN', 'AAPL']
+
+#test_frame = append_data_frame(stock_list)
+
+#plot_prices_over_time(test_frame)
+
+def plot_prices_over_time(data_frame):
+    #note: only have up to 13 distinct colors, so can only plot 13 stocks
+    #to compare at a time ... 
+    colors = ['#33CFA5','orange','#F06A6A','blue', 'violet',
+              'yellowgreen','darkgrey','goldenrod', 'lavenderblush',
+              'fuschia', 'darkolivegreen', 'firebrick', 'blueviolet']
+    data_list = []
+    stock_list = data_frame['symbol'].unique().tolist()
+
+    
+    for i in range(0, len(stock_list)):
+        #subset
+        temp_frame = data_frame[data_frame['symbol'] == stock_list[i]]
+        timestamp_list = pd.to_datetime(temp_frame['timestamp'])
+
+        temp_scatter = go.Scatter(x = timestamp_list,
+                                 y = temp_frame['close'],
+                                 name = stock_list[i],
+                                 line = dict(color = colors[i]))
+        data_list.append(temp_scatter)
+
+    
+    layout = dict(title='Closing Price vs Time Per Stock',
+              showlegend=False,
+              xaxis=dict(
+                    title = 'Time'
+                    ),
+              yaxis=dict(
+                    title = 'Closing Price ($)'
+                    )
+              )
+                  
+    fig = dict(data = data_list, layout = layout)
+    py.iplot(fig, filename = 'closing-prices-over-time')
 
 
 def multi_stock_printout(stock_list):
+    data_list = []
     for stock in stock_list:
         temp_parse = get_stock_data(stock)
+        print('parsed')
         valid_data = validate_stock_data(temp_parse)
         if valid_data == True:
-            temp_frame = create_dataframe(temp_parse)
-            printout(stock, temp_frame)
-            to_csv(stock, temp_frame)
+            print('validated')
+            stock_frame = create_dataframe(stock, temp_parse)
+            data_list.append(stock_frame)
+            print('dataframe created')
+            printout(stock, stock_frame)
+            to_csv(stock, stock_frame)
+            print('to csv done')
+                  
         elif valid_data == False:
             print('Error: could not retrieve ' + stock + 'stock.')
+    
+    multi_stock_frame = append_data_frame(data_list)        
+    plot_prices_over_time(multi_stock_frame)  
+    print('view your plot of closing price vs time at https://plot.ly/~madelinelee/81/closing-price-vs-time-per-stock/#/')      
+
+#multi_stock_printout(stock_list)
 
 def run_robo_advisor():
     stock_list = user_input()
     multi_stock_printout(stock_list)
 
 
-run_robo_advisor()
+#run_robo_advisor()
     
